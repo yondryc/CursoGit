b480049711c4510a88fdad41b236812b87034988
yondryc <yondryc@gmail.com>
2022-06-17 18:42:49 -0400

    Se integro la clase mensaje

--- src/com/main/HolaMundo.java
+++ src/com/main/HolaMundo.java
@@ -3,7 +3,13 @@ package com.main;
 public class HolaMundo {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+
+        Mensaje msj = new Mensaje();
+        String nombre = msj.solicitaDatos("Escribe tu nombre: ");
+        msj.saludar(nombre);
+
+
+
     }
 
 }
---
+++ src/com/main/Mensaje.java
@@ -0,0 +1,19 @@
+package com.main;
+
+import java.util.Scanner;
+
+public class Mensaje {
+
+    public String solicitaDatos(String mensaje){
+        System.out.println(mensaje);
+        Scanner scn = new Scanner(System.in);
+        return scn.nextLine();
+
+    }
+
+    public void saludar(String nombre) {
+
+        System.out.printf("Hola %s, bienvenido a Java \n", nombre);
+
+    }
+}
