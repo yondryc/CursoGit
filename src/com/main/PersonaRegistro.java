25158d7fc8e86d8409fcf48eb05f1b3238fe64b2
yondryc <yondryc@gmail.com>
2022-06-17 19:47:02 -0400

    Se elimino la linea de codigo inservible

--- src/com/main/PersonaRegistro.java
+++ src/com/main/PersonaRegistro.java
@@ -4,8 +4,6 @@ public record PersonaRegistro(String nombre, String apellido) {
 
     @Override
     public String toString() {
-        // Yondry comente la linea de codigo que considero se puede eliminar
-        // System.out.println("Esta instruccion es inservible");
         return "%s %s".formatted(nombre, apellido);
     }
 }
