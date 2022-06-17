9f15a5b8d8986d51632d3718d672c5f148fff23d
yondryc <yondryc@gmail.com>
2022-06-17 19:53:36 -0400

    Se elimino la clase Persona para reemplazarla por el registro Persona

--- src/com/main/Persona.java
+++ src/com/main/Persona.java
@@ -1,29 +1,6 @@
 package com.main;
 
-public class Persona {
-
-    private String nombre,apellido;
-
-    public Persona(String nombre, String apellido) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+public record Persona(String nombre, String apellido) {
 
     @Override
     public String toString() {
--- src/com/main/PersonaRegistro.java
+++
@@ -1,9 +0,0 @@
-package com.main;
-
-public record PersonaRegistro(String nombre, String apellido) {
-
-    @Override
-    public String toString() {
-        return "%s %s".formatted(nombre, apellido);
-    }
-}
